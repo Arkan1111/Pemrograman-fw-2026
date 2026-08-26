c1bf6ba1edf45bbce735ac97f9dc785378d8de20
Muhammad Arkan Hidayat <arkanhidayat107@gmail.com>
2026-08-26 12:17:00 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\DashboardController;
+ 
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
